251659d7af85b8ae2151da222ad3895327fbd5c3
priteshghaywat <87220640+priteshghaywat@users.noreply.github.com>
2024-11-18 14:02:25 +0530

    Update variables.tf

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -1,32 +1,14 @@
-# variables.tf
-variable "client_id" {
-  description = "Azure Client ID (App ID of the Service Principal)"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret (Password of the Service Principal)"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
 variable "resource_group_name" {
-  description = "The name of the Azure resource group"
-  type        = string
-  default     = "my-resource-group"
+  description = "Name of the Azure Resource Group"
+  default     = "myResourceGroup"
 }
 
 variable "location" {
-  description = "The Azure region to create the resource group in"
-  type        = string
+  description = "Azure Region"
   default     = "East US"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
 }
