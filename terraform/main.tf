e6c4d2458f95c1c0e3c6babac5a7301da21267b0
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 02:17:28 +0530

    Check final

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_virtual_network" "vnetcreation2" {
-  name                = "terraform-practice2"
+resource "azurerm_virtual_network" "vnetcreation4" {
+  name                = "terraform-practice4"
   location            = "East US"
   resource_group_name = "azf-rg-tfstate"
   address_space       = ["10.0.0.0/16"]
