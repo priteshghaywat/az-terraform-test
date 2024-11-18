e8d3a0a5a6eb6fe447d3e6afec9959bd4f3a304b
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 01:34:22 +0530

    check 6

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
-terraform {
+/*terraform {
   backend "azurerm" {
     resource_group_name   = "azf-rg-tfstate"          # Resource Group for the Storage Account
     storage_account_name  = "tfstatestorageazfnd"         # Name of the Storage Account
     container_name        = "tfstatecontainer"          # Blob container for storing state files
     key                   = "terraform.tfstate"       # Name of the state file (can be changed per environment)
   }
-}
+}*/
