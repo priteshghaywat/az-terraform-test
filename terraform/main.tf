5bed49580ae6d8e251c4c03ecdd0b8c64d1e9c8b
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-18 22:29:28 +0530

    Created a blob storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,5 @@
-resource "azurerm_storage_account" "storageaccount" {
-  name                     = "tfstatestorageazfnd"
-  resource_group_name      = var.resource_group_name
-  location                 = "East US"
-  account_tier              = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_storage_container" "blobstorage" {
+  name                  = "tfstatecontainer"
+  storage_account_name  = "tfstatestorageazfnd"
+  container_access_type = "private"
 }
