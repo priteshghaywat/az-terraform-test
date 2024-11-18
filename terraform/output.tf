7bf033f59d6125b37cb13a0cb36a1df06a443c47
priteshghaywat <87220640+priteshghaywat@users.noreply.github.com>
2024-11-18 14:37:51 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,10 @@
 # output.tf
 output "resource_group_name" {
   description = "The name of the resource group"
-  value       = azurerm_resource_group.example.name
+  value       = azurerm_resource_group.azf_rg.name
 }
 
 output "resource_group_location" {
   description = "The location of the resource group"
-  value       = azurerm_resource_group.example.location
+  value       = azurerm_resource_group.azf_rg.location
 }
