cac5a9b56c78e4ed5ee3b0f50a4915910b8f5f8b
priteshghaywat <87220640+priteshghaywat@users.noreply.github.com>
2024-11-18 14:01:54 +0530

    Update provider.tf

--- .github/workflows/provider.tf
+++ .github/workflows/provider.tf
@@ -1,10 +1,4 @@
 # provider.tf
 provider "azurerm" {
   features {}
-
-  # Authentication via environment variables (set in GitHub Actions secrets)
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
