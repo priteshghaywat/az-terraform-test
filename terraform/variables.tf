555aedb68b0d004a2719a4b54855aa20c64a092b
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-18 21:59:38 +0530

    Updated Variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
   description = "Name of the Azure Resource Group"
-  default     = var.resource_group_name
+  resource_group_name     = var.resource_group_name
 }
 
 variable "location" {
