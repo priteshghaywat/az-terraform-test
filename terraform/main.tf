07cb027b1c94f391fb4e6d7815d8f7efcb7c296b
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 01:56:41 +0530

    Created a vnet module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_virtual_network" "vnetcreation" {
-  name                = "terraform-practice"
+resource "azurerm_virtual_network" "vnetcreation2" {
+  name                = "terraform-practice2"
   location            = "East US"
   resource_group_name = "azf-rg-tfstate"
   address_space       = ["10.0.0.0/16"]
