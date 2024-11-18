4d669ea9cc6a8db051d2074a1800d33b0e0228fe
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 01:52:41 +0530

    Created a vnet module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
-resource "azurerm_storage_container" "blobstorage" {
-  name                  = "tfstatecontainer"
-  storage_account_name  = "tfstatestorageazfnd"
-  container_access_type = "private"
+resource "azurerm_virtual_network" "vnetcreation" {
+  name                = "terraform-practice"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
 }
