5dbf24e31c09c663905b3cf9049e7557db95dfc0
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 09:03:23 +0530

    Check for the update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,4 @@
-resource "azurerm_virtual_network" "vnetcreation4" {
-  name                = "terraform-practice4"
-  location            = "East US"
-  resource_group_name = "azf-rg-tfstate"
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_resource_group" "azf_rg" {
+  name     = var.resource_group_name
+  location = "East US"
 }
