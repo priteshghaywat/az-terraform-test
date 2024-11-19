e9030ba1af092b29ed725324fdfaffa6b400e42b
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 12:02:25 +0530

    Created  A VNet

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "Vnet" {
   name                = "terraform-practice"
   location            = var.location
-  resource_group_name = "azf-rg-tfstatexy"
+  resource_group_name = "azf-rg-tfstatexyz"
   address_space       = ["10.0.0.0/16"]
 }
