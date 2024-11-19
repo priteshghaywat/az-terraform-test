c5fa7abb99659994afc78af0187e8b8096d3d0c7
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 12:22:20 +0530

    Created A github action

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
-/*terraform {
+terraform {
   backend "azurerm" {
-    resource_group_name   = "azf-rg"          # Resource Group for the Storage Account
-    storage_account_name  = "tfstatestorageazure"         # Name of the Storage Account
+    resource_group_name   = "azf-rg-tfstatexyz"          # Resource Group for the Storage Account
+    storage_account_name  = "tfstatestorageaaz"         # Name of the Storage Account
     container_name        = "tfstatecontainer"          # Blob container for storing state files
     key                   = "terraform.tfstate"       # Name of the state file (can be changed per environment)
   }
-}*/
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_virtual_network" "Vnet" {
-  name                = "terraform-practice"
+resource "azurerm_virtual_network" "Vnet2" {
+  name                = "terraform-practice1"
   location            = var.location
   resource_group_name = "azf-rg-tfstatexyz"
   address_space       = ["10.0.0.0/16"]
