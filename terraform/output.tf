c7f1e18bbfff470b9d781ce6539e797121718dd8
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 11:56:31 +0530

    CHECKED

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,10 @@
 # output.tf
 output "resource_group_name" {
   description = "The name of the resource group"
-  value       = azurerm_resource_group.azf_rg.name
+  value       = azurerm_resource_group.azf_rg_test1.name
 }
 
 output "resource_group_location" {
   description = "The location of the resource group"
-  value       = azurerm_resource_group.azf_rg.location
+  value       = azurerm_resource_group.azf_rg_test1.location
 }
