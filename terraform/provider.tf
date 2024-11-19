11382fdab18a8509a7dbc90919994d2cad1fbaa3
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 10:50:52 +0530

    Added backend.tf file with creds in provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,8 @@
 # provider.tf
 provider "azurerm" {
   features {}
-  subscription_id = "8001f3e8-d729-4cec-9922-ae4c4b0ac050"
+  subscription_id   = "8001f3e8-d729-4cec-9922-ae4c4b0ac050"
+  tenant_id         = "2557041f-ab5b-4b08-806f-27b25d7d69ef"
+  client_id         = "0b050617-a538-47e0-9557-5e4226fbaa67"
+  client_secret     = "w038Q~ekDcuEXp9VjYTWwyR2E~VqhcM-lmk2Cbmm"
 }
