55d69710f68399889186ae85cec0d066eab321f8
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-19 12:33:15 +0530

    Created A github action2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name  = "tfstatestorageaaz"         # Name of the Storage Account
     container_name        = "tfstatecontainer"          # Blob container for storing state files
     key                   = "terraform.tfstate"       # Name of the state file (can be changed per environment)
+    access_key            = "zxHYirCGXgcq6R2/QcSBmLnLJXgmsass94rxzpJnJjES6Pjqg0pToS9LMrwrm6tYB2qQLqtgXyKC+AStt+yLxg=="
   }
 }
