b579f2550083eb092be4f66a1d508926e3a1788e
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-27 14:33:59 +0530

    test

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name   = "azf-rg-tfstatexyz"          # Resource Group for the Storage Account
     storage_account_name  = "tfstatestorageaaz"         # Name of the Storage Account
     container_name        = "tfstatecontainer"          # Blob container for storing state files
-    access_key            = "zxHYirCGXgcq6R2/QcSBmLnLJXgmsass94rxzpJnJjES6Pjqg0pToS9LMrwrm6tYB2qQLqtgXyKC+AStt+yLxg=="
+    #access_key            = "zxHYirCGXgcq6R2/QcSBmLnLJXgmsass94rxzpJnJjES6Pjqg0pToS9LMrwrm6tYB2qQLqtgXyKC+AStt+yLxg=="
     key                   = "terraform.tfstate"       # Name of the state file (can be changed per environment)
   }
 }
