91d7bfd29b87350993e0b7ffed703ee260f044ac
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-27 14:36:45 +0530

    test2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,6 +4,7 @@ terraform {
     storage_account_name  = "tfstatestorageaaz"         # Name of the Storage Account
     container_name        = "tfstatecontainer"          # Blob container for storing state files
     #access_key            = "zxHYirCGXgcq6R2/QcSBmLnLJXgmsass94rxzpJnJjES6Pjqg0pToS9LMrwrm6tYB2qQLqtgXyKC+AStt+yLxg=="
+    use_azuread_auth      = true
     key                   = "terraform.tfstate"       # Name of the state file (can be changed per environment)
   }
 }
