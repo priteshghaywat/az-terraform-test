df5715dd7bccd1d06f686bf6c6d67a196ed48d5c
Pritesh Ghaywat <niteshagarwal5560@gmail.com>
2024-11-27 16:51:23 +0530

    updated provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,7 @@
 # provider.tf
 provider "azurerm" {
   features {}
-  use_oidc = true
+  use_msi = true
+  tenant_id = "2557041f-ab5b-4b08-806f-27b25d7d69ef"
   subscription_id   = "8001f3e8-d729-4cec-9922-ae4c4b0ac050"
 }
